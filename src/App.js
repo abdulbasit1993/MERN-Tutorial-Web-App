a9cc9f812ad40a5b1e377504bb4940bb342fcdd0
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2022-04-10 21:09:26 +0500

    Created Responsive Navbar Menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World from AB</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 };
 
